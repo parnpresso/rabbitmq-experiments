48d3ad307555dace52784426c84595c8ba4fe11c
Parn Boonyamanee <parnpresso@gmail.com>
2020-04-19 15:36:54 +0700

    ✨ Add Work queue (AKA. Task queue) experiment :crown:

---
+++ 2 - Work queue/consumer_charlie.py
@@ -0,0 +1,25 @@
+import time
+
+import pika
+
+
+# Make a connection
+connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+channel = connection.channel()
+
+# Declare queue
+channel.queue_declare(queue='postbox')
+
+# Consume data from queue
+def callback(ch, method, properties, body):
+    print(f'Received: {body}')
+    time.sleep(body.count(b'.'))
+    print('Done')
+
+channel.basic_consume(
+    queue='postbox',
+    auto_ack=True,
+    on_message_callback=callback,
+)
+
+channel.start_consuming()
